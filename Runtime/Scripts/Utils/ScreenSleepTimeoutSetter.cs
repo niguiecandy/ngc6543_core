2018933112dedaf7d1834af61dceaa3b59da2175
Chaehyeok Lim <NGC6543@me.com>
2020-01-03 14:20:34 +0900

    gitflow-feature-stash: screen-sleep-timeout

---
+++ Runtime/Scripts/Utils/ScreenSleepTimeoutSetter.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace NGC6543
+{
+	public class ScreenSleepTimeoutSetter : MonoBehaviour
+	{
+		const string LOG_HEADER = "ScreenSleepTimeoutController : ";
+		
+		enum ScreenSleepTimeout
+		{
+			NeverSleep,
+			
+			SystemSettings
+		}
+		
+		[SerializeField] ScreenSleepTimeout _sleepTimeout = ScreenSleepTimeout.NeverSleep;
+		
+		
+		void Start()
+		{
+			Debug.Log(LOG_HEADER + "Setting Screen Sleep Timeout to : " + _sleepTimeout);
+			switch (_sleepTimeout)
+			{
+				case ScreenSleepTimeout.NeverSleep :
+					Screen.sleepTimeout = SleepTimeout.NeverSleep;
+					break;
+				case ScreenSleepTimeout.SystemSettings :
+					Screen.sleepTimeout = SleepTimeout.SystemSetting;
+					break;
+			}
+		}
+		
+	}
+}
